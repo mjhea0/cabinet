eba5da5a89555a9e429e82288f71ca27861114f4
Michael Herman <hermanmu@gmail.com>
2015-01-15 16:45:15 -0700

    added base tests

---
+++ tests/test_main.py
@@ -0,0 +1,19 @@
+# tests/test_main.py
+
+
+import unittest
+
+from base import BaseTestCase
+
+
+class TestMainBlueprint(BaseTestCase):
+
+    def test_index(self):
+        # Ensure Flask is setup.
+        response = self.client.get(
+            '/', content_type='html/text', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
