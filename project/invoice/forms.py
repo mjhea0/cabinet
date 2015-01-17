3e12a4b81918dc875f5cc2069a08299b29511430
Michael Herman <hermanmu@gmail.com>
2015-01-16 22:26:47 -0700

    updated blueprints

--- project/invoice/forms.py
+++ project/invoice/forms.py
@@ -1,11 +1,16 @@
 # project/invoice/forms.py
 
 
+import datetime
+
 from flask_wtf import Form
 from wtforms import FloatField, SelectField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
 
 
 class AddInvoiceForm(Form):
     client = SelectField('Client', coerce=int)
     total_price = FloatField('Total Price', validators=[DataRequired()])
+    invoice_date = DateField(
+        'Invoice Date', format='%Y-%m-%d', default=datetime.datetime.now())
